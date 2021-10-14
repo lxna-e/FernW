57a2b4ed81af3f119d8528bd0861e99d1911bf84
lxna-e <lena-oswald@gmx.net>
2021-10-14 09:55:57 +0200

    Fehlerbehebung

--- fernw-app/src/components/Header/LeftHeaderLinks.js
+++ fernw-app/src/components/Header/LeftHeaderLinks.js
@@ -47,41 +47,103 @@ export default function LeftHeaderLinks(props) {
             className: classes.navLink,
             color: "transparent",
           }}
+          // noLiPadding
           dropdownList={[
-             <Link to={"/unterkunft/hotel"} className={classes.listItem}>
+            <Link to={"/unterkunft/hotel"} className={classes.dropdownLink}>
               Hotel
             </Link>,
-            <Link to={"/unterkunft/ferienwohnung"} className={classes.listItem} >
+            <Link to={"/unterkunft/ferienwohnung"} className={classes.dropdownLink} >
               Ferienwohnung
             </Link>,
-            <Link to={"/unterkunft/appartement"} className={classes.listItem}>
+            <Link to={"/unterkunft/appartement"} className={classes.dropdownLink}>
               Appartement
             </Link>,
-            <Link to={"/unterkunft/kreuzfahrtschiff"} className={classes.listItem}>
+            <Link to={"/unterkunft/kreuzfahrtschiff"} className={classes.dropdownLink}>
               Kreuzfahrtschiff
             </Link>,
             { divider: true },
-            <Link to={"/unterkunft"} className={classes.listItem}>
+            <Link to={"/unterkunft"} className={classes.dropdownLink}>
               alle Unterkünfte
             </Link>,
           ]}
         />
       </ListItem>
       <ListItem className={classes.listItem}>
-        <Link to={"/reisemittel"} className={classes.navLink}>
-          Reisemittel
-        </Link>
+        <CustomDropdown
+          buttonText="Reisemittel"
+          dropdownHeader="Reisemittel:"
+          buttonProps={{
+            className: classes.navLink,
+            color: "transparent",
+          }}
+          // noLiPadding
+          dropdownList={[
+            <Link to={"/reisemittel/flug"} className={classes.dropdownLink}>
+              FLug
+            </Link>,
+            <Link to={"/reisemittel/bahn"} className={classes.dropdownLink} >
+              Bahn
+            </Link>,
+            <Link to={"/reisemittel/bus"} className={classes.dropdownLink}>
+              Bus
+            </Link>,
+            { divider: true },
+            <Link to={"/reisemittel"} className={classes.dropdownLink}>
+              alle Reisemittel
+            </Link>,
+          ]}
+        />
       </ListItem>
       <ListItem className={classes.listItem}>
-        <Link to={"/aktivitaeten"} className={classes.navLink}>
-          Aktivitäten
-        </Link>
+        <CustomDropdown
+          buttonText="Aktivitäten"
+          dropdownHeader="Aktivitäten:"
+          buttonProps={{
+            className: classes.navLink,
+            color: "transparent",
+          }}
+          // noLiPadding
+          dropdownList={[
+            <Link to={"/aktivitaeten/stadttour"} className={classes.dropdownLink}>
+              Stadttour
+            </Link>,
+            <Link to={"/aktivitaeten/wanderung"} className={classes.dropdownLink} >
+              Wanderung
+            </Link>,
+            { divider: true },
+            <Link to={"/aktivitaeten"} className={classes.dropdownLink}>
+              alle Aktivitäten
+            </Link>,
+          ]}
+        />
       </ListItem>
       <ListItem className={classes.listItem}>
-        <Link to="/pauschalreisen" className={classes.navLink}>
-          Pauschalreisen
-        </Link>
-      </ListItem>
+      <CustomDropdown
+      buttonText="Pauschalreisen"
+      dropdownHeader="Pauschalreisen:"
+      buttonProps={{
+      className: classes.navLink,
+      color: "transparent",
+    }}
+      // noLiPadding
+      dropdownList={[
+      <Link to={"/pauschalreisen/bus"} className={classes.dropdownLink}>
+      Bus-Pauschalreisen
+      </Link>,
+      <Link to={"/pauschalreisen/winter"} className={classes.dropdownLink} >
+      Winterurlaube
+      </Link>,
+      <Link to={"/pauschalreisen/strand"} className={classes.dropdownLink}>
+      Strandurlaub
+      </Link>,
+
+    { divider: true },
+      <Link to={"/pauschalreisen"} className={classes.dropdownLink}>
+      alle Pauschalreisen
+      </Link>,
+    ]}
+      />
+
     </List>
   );
 }
